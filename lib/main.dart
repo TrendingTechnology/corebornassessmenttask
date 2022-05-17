20c2202baa4e56db1de49fc5a039d40bb3f7d993
emirhalici <emir.halici1@gmail.com>
2022-05-17 23:46:09 +0300

    implement provider and api fetching

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:corebornassessmenttask/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/main_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MainProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/providers/main_provider.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+import 'package:corebornassessmenttask/models/album_model.dart';
+import 'package:corebornassessmenttask/models/comment_model.dart';
+import 'package:corebornassessmenttask/models/post_model.dart';
+import 'package:corebornassessmenttask/models/user_model.dart';
+import 'package:corebornassessmenttask/services/api_service.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class MainProvider with ChangeNotifier {
+  List<PostModel> posts = [];
+  List<CommentModel> postComments = [];
+  List<AlbumModel> allAlbums = [];
+  List<UserModel> users = [];
+
+  Future<String> getPosts() async {
+    try {
+      Response response = await ApiService.getPosts();
+      posts = [];
+      var json = jsonDecode(response.body);
+      for (var element in json) {
+        PostModel post = PostModel.fromJson(element);
+        posts.add(post);
+      }
+      notifyListeners();
+      return '0';
+    } catch (e) {
+      print('Exception while getting posts: $e');
+      return e.toString();
+    }
+  }
+
+  Future<String> getCommentsForPost(int postId) async {
+    try {
+      Response response = await ApiService.getComments(postId);
+      postComments = [];
+      var json = jsonDecode(response.body);
+      for (var element in json) {
+        CommentModel comment = CommentModel.fromJson(element);
+        postComments.add(comment);
+      }
+      notifyListeners();
+      return '0';
+    } catch (e) {
+      print('Exception while getting comments for post $postId: $e');
+      return e.toString();
+    }
+  }
+
+  Future<String> getAlbums() async {
+    try {
+      Response response = await ApiService.getAlbums();
+      allAlbums = [];
+      var json = jsonDecode(response.body);
+      for (var element in json) {
+        AlbumModel albumModel = AlbumModel.fromJson(element);
+        allAlbums.add(albumModel);
+      }
+      notifyListeners();
+      return '0';
+    } catch (e) {
+      print('Exception while getting albums: $e');
+      return e.toString();
+    }
+  }
+
+  Future<String> getUsers() async {
+    try {
+      Response response = await ApiService.getUsers();
+      users = [];
+      var json = jsonDecode(response.body);
+      for (var element in json) {
+        UserModel user = UserModel.fromJson(element);
+        users.add(user);
+      }
+      notifyListeners();
+      return '0';
+    } catch (e) {
+      print('Exception while getting users: $e');
+      return e.toString();
+    }
+  }
+}
