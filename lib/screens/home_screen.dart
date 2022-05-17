b8e4aad08936049ca66b679c920d9e2eb32afaf5
emirhalici <emir.halici1@gmail.com>
2022-05-18 01:07:03 +0300

    implement main page posts with listtile

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,10 @@
+import 'package:corebornassessmenttask/models/post_model.dart';
+import 'package:corebornassessmenttask/providers/main_provider.dart';
+import 'package:corebornassessmenttask/utils/project_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:http/http.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +14,153 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void showSnackBarMessage(String message) => ProjectUtils.showSnackBarMessage(context, message);
+
+  @override
+  void initState() {
+    fetchPost();
+    super.initState();
+    _scrollController.addListener(
+      () {
+        if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent && !loading && !allLoaded) {
+          print('new items are loaded');
+          fetchPost();
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  List<PostModel> posts = [];
+  bool loading = false, allLoaded = false;
+  final ScrollController _scrollController = ScrollController();
+
+  fetchPost() async {
+    setState(() {
+      loading = true;
+    });
+    List<PostModel> sourcePosts = context.read<MainProvider>().posts;
+    if (sourcePosts.isEmpty) {
+      String response = await context.read<MainProvider>().getPosts();
+      print(response);
+      if (response != '0') {
+        showSnackBarMessage(response);
+        setState(() {
+          loading = false;
+        });
+        return;
+      } else {
+        sourcePosts = context.read<MainProvider>().posts;
+      }
+      print('source posts: ${sourcePosts.length}');
+    }
+    if (posts.length == sourcePosts.length) {
+      setState(() {
+        allLoaded = true;
+      });
+    } else {
+      setState(
+        () {
+          if (sourcePosts.length - posts.length < 8) {
+            sourcePosts.getRange(posts.length, sourcePosts.length).forEach(
+              (element) {
+                posts.add(element);
+              },
+            );
+          } else {
+            sourcePosts.getRange(posts.length, posts.length + 8).forEach(
+              (element) {
+                posts.add(element);
+              },
+            );
+          }
+        },
+      );
+    }
+    setState(() {
+      loading = false;
+    });
+    print('posts: ${posts.length}');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: Center(child: Text('Hello world')),
+        appBar: AppBar(
+          title: const Text('Assessment App'),
+          centerTitle: false,
+          actions: [
+            ElevatedButton.icon(
+              onPressed: () {
+                // TODO : REDIRECT TO USERS PAGE
+                //Navigator.push(context, MaterialPageRoute(builder: (context) => const UsersScreen()));
+              },
+              icon: Icon(
+                Icons.contacts_sharp,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+              label: Text(
+                'Users',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: posts.isEmpty
+            ? ProjectUtils.circularProgressBar(context)
+            : Stack(
+                children: [
+                  ListView.separated(
+                    controller: _scrollController,
+                    itemCount: posts.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          title: Text(
+                            posts[index].title,
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                            ),
+                          ),
+                          subtitle: Text(
+                            posts[index].body,
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      if (index == posts.length - 1) {
+                        return Container();
+                      } else {
+                        return const Divider(height: 1);
+                      }
+                    },
+                  ),
+                  if (loading)
+                    Positioned(
+                      left: 0,
+                      bottom: 0,
+                      child: SizedBox(
+                        height: 80.h,
+                        width: MediaQuery.of(context).size.width,
+                        child: ProjectUtils.circularProgressBar(context),
+                      ),
+                    ),
+                ],
+              ),
       ),
     );
   }
